12338e9fb441c61ffbf7e586611a9f411f722a01
StefyAvila <stefany@unah.hn>
2022-09-29 16:37:00 -0600

    agregue un label

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,16 @@
+@extends('plantillas.plantilla')
+
+@section('title', 'Home')
+
+@section('content')
+        <h1>Esta es una prueba basica</h1>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Cumque porro minus amet deserunt qui consequuntur quis consequatur maxime aut quod tempore 
+      
+                incidunt possimus vel consectetur, n
+                obis iusto rem nisi quae.</p>
+
+                <label for="">Holaaa</label>
+@endsection
+    
